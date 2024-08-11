5531f1508f4484a1ab9737c895385c16e41b3aa3
Linda-Gao <lindakaompe@hotmail.com>
2024-08-10 23:23:48 -0400

    HomeControllerTest.java

---
+++ src/test/java/com/cst8277/spring_social_2_cloud/HomeControllerTest.java
@@ -0,0 +1,22 @@
+package com.cst8277.spring_social_2_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class HomeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldDisplayHomePage() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isOk());
+
+    }
+}
