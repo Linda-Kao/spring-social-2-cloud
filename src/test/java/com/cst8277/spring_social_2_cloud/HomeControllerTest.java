0e31ae04679dc431fb2aeb649a23a8111e9d9363
Linda-Gao <lindakaompe@hotmail.com>
2024-08-11 01:20:56 -0400

    SecurityConfiguration.java index.html HomeControllerTest.java pom.xml

--- src/test/java/com/cst8277/spring_social_2_cloud/HomeControllerTest.java
+++ src/test/java/com/cst8277/spring_social_2_cloud/HomeControllerTest.java
@@ -3,6 +3,7 @@ package com.cst8277.spring_social_2_cloud;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,8 +16,8 @@ class HomeControllerTest {
     MockMvc mockMvc;
 
     @Test
-    void shouldDisplayHomePage() throws Exception {
+    @WithMockUser
+    void authenticatedUserShouldDisplayHomePage() throws Exception {
         mockMvc.perform(get("/")).andExpect(status().isOk());
-
     }
 }
