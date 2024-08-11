aca40147bfb3b66c12a76a3895430c4d923afab9
Linda-Gao <lindakaompe@hotmail.com>
2024-08-10 22:17:05 -0400

    Initial Commit

---
+++ src/main/java/com/cst8277/spring_social_2_cloud/SpringSocial2CloudApplication.java
@@ -0,0 +1,13 @@
+package com.cst8277.spring_social_2_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocial2CloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocial2CloudApplication.class, args);
+	}
+
+}
